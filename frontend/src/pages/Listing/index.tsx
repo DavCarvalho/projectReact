709d4effa9287d8379ab40e0537161bfd2602b1c
Davi Carvalho <davircsp@hotmail.com>
2022-03-12 13:06:55 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,23 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
-
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
+  //Hooks são funções cujo comportamento está vinculado ao estado e ao ciclo de vida do React a partir de componentes funcionais.
+  const [pageNumber, setPageNumber] = useState(0) //Manter estado no componente
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}//movies?size=12&page=0`)
+      .then(response => {
+        const data = response.data as MoviePage
+        setPageNumber(data.number)
+      })
+  }, [])
+  //Executar algo na instanciação ou destruição do componente, observar estado
+
 
   return (
     <>
