24fbce2471bc424d46d513529d8177a9a955bb79
Davi Carvalho <davircsp@hotmail.com>
2022-03-12 14:01:01 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,13 +9,25 @@ function Listing() {
   //Hooks são funções cujo comportamento está vinculado ao estado e ao ciclo de vida do React a partir de componentes funcionais.
   const [pageNumber, setPageNumber] = useState(0) //Manter estado no componente
 
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true
+  })
+
   useEffect(() => {
-    axios.get(`${BASE_URL}//movies?size=12&page=0`)
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
       .then(response => {
         const data = response.data as MoviePage
-        setPageNumber(data.number)
+        setPage(data)
       })
-  }, [])
+  }, [pageNumber])
   //Executar algo na instanciação ou destruição do componente, observar estado
 
 
@@ -25,18 +37,15 @@ function Listing() {
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+          {page.content.map(movie => (
+
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          )
+          )}
+
+
         </div>
       </div>
 
